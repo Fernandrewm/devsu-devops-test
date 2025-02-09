e21074d0fc1712b44acd4561bb6ddd0fe1ce7c50
Fernando Muñoz <fernandrewm@gmail.com>
2025-02-09 00:20:46 -0600

    fix to applied some good python practices

--- devsu-demo-devops-python/api/urls.py
+++ devsu-demo-devops-python/api/urls.py
@@ -1,6 +1,5 @@
-from .views import *
-from django.urls import path
 from rest_framework import routers
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, 'users')
