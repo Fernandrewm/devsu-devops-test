a6ccf75b50764530632efb786fa9ed0ab0377b9e
Fernando Muñoz <fernandrewm@gmail.com>
2025-02-09 21:56:51 -0600

    fix

--- devsu-demo-devops-python/demo/settings.py
+++ devsu-demo-devops-python/demo/settings.py
@@ -137,3 +137,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Configuración del directorio temporal
+TEMP_DIR = '/tmp'
+FILE_UPLOAD_TEMP_DIR = TEMP_DIR
