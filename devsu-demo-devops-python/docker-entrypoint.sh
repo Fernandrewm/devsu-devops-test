c3ab7e3a8bf8560203343f0ff56ad7f7e4fd7012
Fernando Muñoz <fernandrewm@gmail.com>
2025-02-10 05:04:26 -0600

    update

--- devsu-demo-devops-python/docker-entrypoint.sh
+++ devsu-demo-devops-python/docker-entrypoint.sh
@@ -3,14 +3,11 @@ set -e
 
 cd $APP_HOME
 
-# Esperar a que PostgreSQL esté disponible
 echo "Waiting for PostgreSQL to be ready..."
 sleep 5
 
-# Inicializar la base de datos
 echo "Running migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
-# Iniciar el servidor
 exec python manage.py runserver 0.0.0.0:8000
