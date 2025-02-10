00a090ac88ee52a54226b93874561d2dfecf531a
Fernando Muñoz <fernandrewm@gmail.com>
2025-02-09 22:11:52 -0600

    fix

--- devsu-demo-devops-python/docker-entrypoint.sh
+++ devsu-demo-devops-python/docker-entrypoint.sh
@@ -6,14 +6,12 @@ DATA_DIR=${DATA_DIR:-"/app/data"}
 APP_HOME=${APP_HOME:-"/app"}
 
 mkdir -p $DATA_DIR
-chmod 755 $DATA_DIR
 
 sleep 5
 
 if [ ! -f "$DATA_DIR/db.sqlite3" ]; then
   echo "Initializing database..."
   touch "$DATA_DIR/$DB_NAME"
-  chmod 644 "$DATA_DIR/$DB_NAME"
   ln -sf "$DATA_DIR/$DB_NAME" "$APP_HOME/$DB_NAME"
   python manage.py makemigrations
   python manage.py migrate
